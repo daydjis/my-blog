6264abe7c7ccd6d50b0817c62923f470cffc8fc9
dayji <ilia.mironyuk21@gmail.com>
2023-03-26 14:20:46 +0400

    ts

--- my-blog/src/main.tsx
+++ my-blog/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
 import App from './index.js';
 import './index.css';
-import store from './store/store.js';
+import { setupStore } from './store/store.js';
+const store = setupStore()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
--- my-blog/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import weatherSlice from './weather/weatherSlice'
-
-export default configureStore({
-    reducer: {
-        weather: weatherSlice,
-    },
-})
